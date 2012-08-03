51d788aed83d5bc1348f182c4c3cca6b7edea22a
Amit Upadhyay <comments@amitu.com>
2012-08-03 15:57:12 -0400

    minor readme etc

--- foo.py
+++ foo.py
@@ -1,58 +1,34 @@
-# foo.py
+#!/usr/bin/env python
 from amitu import d
 
-# key advantages:
-# 1. single file app
-# 2. still fully compatible with existing django projects
-# 3. auto matic url regex creation
-# 4. all frequently used django functions/classes collected in d.* namespace
-# Note: not yet sure if all of this is possible :-p
+d(DEBUG=True) # configure django
 
-d(							# callable modules are magic. some magic is good.
-	DEBUG = True,
-	urls = (
-		("^admin/", d.include("django.contrib.auth")), 
-	),
-	INSTALLED_APPS = ("django.contrib.admin", "foo"),
-	TEMPLATES_DIR = [d.dotslash("templates")],
-	regexers = {
-		"domain": regex_for_domain,
-	}
+def real_index2(request):
+    return d.HttpResponse("real_index2")
+    
+d(      # configure other urlpatterns
+    d.patterns("",
+        ("^$", real_index2),
+    )
 )
 
-from foo.models import Foo 	   # define custom models in models.py as usual
-
-@d("/", name="index") # named url
+@d # /index/
 def index(request):
-	return "index.html", {} # {} optional
+    import time
+    return "index.html", {"msg": time.time()}
 
-@d 		# equivalent of @d("/whoami/")
-def whoami(request):
-	return d.HttpResponse("amitu")
+@d
+def real_index(request):
+    return "home.html"
+    
+@d
+def json(request):
+    return {"sum": int(request.GET.get("x", 0)) + int(request.GET.get("y", 0))}
 
-@d("/<username>/", "/user/<username>") # both urls point to same view
-def user(request, username):
-	if request.path.startswith("/user/"): 
-		return d.HttpResponseRedirect("/%s/" % username)
-	# lots of django helpers available with d.something
-	foo = d.get_object_or_404(Foo, username=username)
-	return "user.html", {"foo": foo}
-
-# things in <> can have type too, sytax is <type:name>
-@d("/blog/<int4:year>/<word:month>/<int2:day>/<slug:title>/") # auto regexes
-def blog_post(request, year, month, day, title):
-	pass
-	
-@d("/domain/<domain:site>") 	# custom types can be defined in setting call
-def show_domain(request, site):
-	pass
-	
-@d("/do-something", template="some-form.html") # fhurl integrated
-class SomeForm(d.RequestForm):
-	somefield = d.forms.CharField()
-	def save():
-		pass
-	
-if __name__ == "__main__":
-	#d("shell")
-	d("runserver", 8000, host="foo") # can also read host:port from sys.argv
+@d("^fhurl/$")
+class MyForm(d.RequestForm):
+    x = d.forms.IntegerField()
+    y = d.forms.IntegerField()
+    
+    def save(self):
+        return self.cleaned_data["x"] + self.cleaned_data["y"]
