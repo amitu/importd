22dca353a1d333d8ffb3506fc95d5ec67fdaa40a
Amit Upadhyay <comments@amitu.com>
2012-10-22 21:06:02 +0530

    version 0.1.4, setup.py was buggy

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+try:
+    long_description=open('README.rst', 'rt').read(),
+except Exception:
+    long_description=""
+
 setup(
     name = "importd",
     description = "a django based miniframework, inspired by sinatra",
-    long_description=open('README.rst', 'rt').read(),
+    long_description=long_description,
 
-    version = "0.1.2",
+    version = "0.1.4",
     author = 'Amit Upadhyay',
     author_email = "upadhyay@gmail.com",
 
