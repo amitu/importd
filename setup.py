14775003488f32f8ea800b87b4ca0775989f4531
Amit Upadhyay <comments@amitu.com>
2013-02-25 11:23:12 -0500

    setup.py issue, closes #5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 try:
-    long_description=open('README.rst', 'rt').read(),
+    long_description=open('README.rst', 'rt').read()
 except Exception:
     long_description=""
 
