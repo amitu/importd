a58435528e65baa7cce2841e2450058edfa53ed1
Amit Upadhyay <amitu@browserstack.com>
2014-10-27 22:29:15 +0530

    0.3.0 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="a django based miniframework, inspired by sinatra",
     long_description=long_description,
 
-    version="0.2.9",
+    version="0.3.0",
     author='Amit Upadhyay',
     author_email="upadhyay@gmail.com",
 
