3b99493e606a04a6338d8ee2fc299595d19b2a44
juancarlospaco <juancarlospaco@gmail.com>
2015-04-11 18:51:46 -0300

    Add BzTar and EGG format to Fabric script

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
 from fabric.api import local, cd
+
 
 def docs():
     local("./bin/docs")
     local("./bin/python setup.py upload_sphinx --upload-dir=docs/html")
 
+
 def release():
-    # update version id in setup.py, changelog and docs/source/conf.py
-    local("python setup.py sdist --formats=gztar,zip upload")
+    """Update version id in setup.py, changelog and docs/source/conf.py."""
+    local(("python setup.py bdist_egg sdist --formats=bztar,gztar,zip "
+           "upload --show-response"))
